0f781aff30628a1bcd843b0e7c4b113d8861e042
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-22 18:02:24 +0600

    banner sections button updated for medium device

--- src/Components/Banner-section/Banner.jsx
+++ src/Components/Banner-section/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
            <h1 className='text-4xl font-bold'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
            <p className='text-2xl text-gray-300'>Beyond Boundaries Beyond Limits</p>
           <div className='border-2 border-[#E7FE29] p-2 inline-block rounded-xl cursor-pointer'>
-             <button className='bg-[#E7FE29] text-black px-4 py-3 rounded-lg font-semibold cursor-pointer'>Claim Free Credit</button>
+             <button className='bg-[#E7FE29] text-black px-4 md:px-10 py-3 rounded-lg font-semibold cursor-pointer'>Claim Free Credit</button>
           </div>
         </div>
     );
